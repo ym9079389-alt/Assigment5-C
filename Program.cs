d5884a89c3841af73cb34f145b182fdbacc666eb
ym9079389-alt <ym9079389@gmail.com>
2026-09-06 10:14:57 +0300

    Q 1

--- Program.cs
+++ Program.cs
@@ -19,70 +19,7 @@ namespace Assigment5_C_
             // ظهر  compile error لان بالنسباله مفيش متغير بالاسم ده لانه متعرف private
             #endregion
 
-            #region Q 2
-            //Add an internal int copiesInStock = 5; field to Book.Print it from Main.Does it compile? Why ?
-            //Console.WriteLine(book.copiesInStock);
-            // ظهر 5 لان internal متعرفه داخل نفس المشروع
-            #endregion
-
-            #region Q 3
-            //Add a public string Title; field to Book.Set it and print it from Main.
-            //Console.Write("Enter the title of the book:");
-            //book.Title = Console.ReadLine();
-            //Console.WriteLine(book.Title);
-            #endregion
-
-            #region Q 4
-            //Declare an enum Genre { Fiction, NonFiction, Science }. Add a Genre property to Book, assign it Genre.Science, and print it.
-            //book.Genre = Genre.Science;
-            //Console.WriteLine(book.Genre);
-            #endregion
-
-            #region Q 5
-            //Using the Genre enum above, print the underlying int value of Genre.Fiction, Genre.NonFiction, and Genre.Science by casting each to int.
-            //int fictionValue = (int) Genre.Fiction;
-            //int nonFictionValue = (int) Genre.NonFiction;
-            //int scienceValue = (int) Genre.Science;
-            //Console.WriteLine(fictionValue);
-            //Console.WriteLine(nonFictionValue);
-            //Console.WriteLine(scienceValue);
-            #endregion
-
-            #region Q 6
-            //Given int genreNumber = 1;, cast it into a Genre value and print the result.
-            //int genreNumber = 1;
-            //Genre genre = (Genre)genreNumber;
-            //Console.WriteLine(genre);
-            #endregion
-
-            #region Q 7
-            //Given Genre genre = Genre.Fiction;, convert it into a string using ToString() and print it.
-            //Genre genre = Genre.Fiction;
-            //string genreString = genre.ToString();
-            //Console.WriteLine(genreString);
-            #endregion
-
-            #region Q 8
-            //Given string genreText = "Science";, convert it into a Genre value using Enum.Parse() and print the result.
-            //string genreText = "Science";
-            //Genre genre = (Genre)Enum.Parse(typeof(Genre), genreText);
-            //Console.WriteLine(genre);
-            #endregion
-
-            #region Q 9
-            //Given string genreText = "Mystery"; (not a valid Genre value), use Enum.TryParse() to attempt the conversion.Print "Unknown genre" if it fails.
-            string genreText = "Mystery";
-            bool isParsed;
-            isParsed = Enum.TryParse<Genre>(genreText, out Genre genre);
-            if (isParsed)
-            {
-                Console.WriteLine(genre);
-            }
-            else
-            {
-                Console.WriteLine("Unknown genre");
-            }
-            #endregion
+           
         }
     }
 }
