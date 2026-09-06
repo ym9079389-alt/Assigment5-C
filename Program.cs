5bbbae3782055ae60c8921f1647305c56030a71e
ym9079389-alt <ym9079389@gmail.com>
2026-09-06 08:59:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Specialized;
+using System.Runtime.Serialization.Formatters;
+
+namespace Assigment5_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
